001cbb9fccb4140f978928d84d2ac81f358e354c
mihirnayak66 <79975126+mihirnayak66@users.noreply.github.com>
2021-03-25 13:08:45 +0530

    Create sframe.java

---
+++ sframe.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class sframe
+{
+ public static void main(String[] args)
+ {
+  JFrame obj=new JFrame();
+  obj.setSize(300,300);
+  obj.setVisible(true);
+  obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+  }
